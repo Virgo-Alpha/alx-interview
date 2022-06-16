af7073df4f580fa7cc259a76aff003cb8be42a15
Virgo Alpha <bensonkingori96@gmail.com>
2022-06-16 10:21:36 +0300

    light

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,9 +6,24 @@ def isWinner(x, nums):
     """returns the name of winner"""
     if not nums or x < 1:
         return None
-
-    players = ["Maria", "Ben"]
-    for i in range(len(nums)):
-        if nums[i] % 2 == 0:
-            return players[1]
-        return players[0]
+    n = max(nums)
+    fltr = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not fltr[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            fltr[j] = False
+    fltr[0] = fltr[1] = False
+    c = 0
+    for i in range(len(fltr)):
+        if fltr[i]:
+            c += 1
+        fltr[i] = c
+    plyr1 = 0
+    for n in nums:
+        plyr1 += fltr[n] % 2 == 1
+    if plyr1 * 2 == len(nums):
+        return None
+    if plyr1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
